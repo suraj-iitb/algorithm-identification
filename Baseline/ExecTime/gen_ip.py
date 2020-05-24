ba672e2a5a3cbd61c2125af39e350e7942901a6a
suraj <suraj.iitb.in@gmail.com>
2020-05-24 18:27:07 +0530

    Exec time comparisons for merge and heap sort

---
+++ Baseline/ExecTime/gen_ip.py
@@ -0,0 +1,12 @@
+steps = 1
+n_list = []
+
+for _ in range(1, 4):
+    steps = steps * 10
+    n_list.append(steps)
+
+for n in n_list:
+    with open('input-'+str(n), 'w') as f:
+        f.write(str(n))
+        for i in range (n, 0, -1):
+            f.write(' '+str(i))
---
+++ Baseline/ExecTime/run.py
@@ -0,0 +1,25 @@
+import os
+from subprocess import Popen, PIPE
+import subprocess
+
+os.system("g++ -o heap_sort merge-sort.cpp")
+os.system("g++ -o merge_sort heap-sort.cpp")
+
+steps = 1
+n_list = []
+
+for _ in range(1, 4):
+    steps = steps * 10
+    n_list.append(steps)
+
+for n in n_list:
+    subprocess.Popen([os.path.join(os.getcwd(), 'merge_sort')],
+           stdin=open('input-'+str(n)),
+           stdout=subprocess.PIPE, 
+           stderr=subprocess.STDOUT,
+           shell=True)
+    subprocess.Popen([os.path.join(os.getcwd(), 'heap_sort')],
+           stdin=open('input-'+str(n)),
+           stdout=subprocess.PIPE, 
+           stderr=subprocess.STDOUT,
+           shell=True)

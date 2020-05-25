287e839ed9a301ddeab9b761c6278d14f2a0eb86
suraj <suraj.iitb.in@gmail.com>
2020-05-25 09:07:02 +0530

    merge sort for worst case

--- Baseline/ExecTime/merge-sort.cpp
+++ Baseline/ExecTime/merge-sort.cpp
@@ -86,6 +86,53 @@ void printArray(int A[], int size)
     printf("\n"); 
 } 
   
+int join(int arr[], int left[], int right[], 
+          int l, int m, int r) 
+{ 
+    int i; // Used in second loop 
+    for (i = 0; i <= m - l; i++) 
+        arr[i] = left[i]; 
+  
+    for (int j = 0; j < r - m; j++) 
+        arr[i + j] = right[j]; 
+} 
+  
+// Function to store alternate elemets in left 
+// and right subarray 
+int split(int arr[], int left[], int right[], 
+          int l, int m, int r) 
+{ 
+    for (int i = 0; i <= m - l; i++) 
+        left[i] = arr[i * 2]; 
+  
+    for (int i = 0; i < r - m; i++) 
+        right[i] = arr[i * 2 + 1]; 
+} 
+  
+// Function to generate Worst Case of Merge Sort 
+int generateWorstCase(int arr[], int l, int r) 
+{ 
+    if (l < r) 
+    { 
+        int m = l + (r - l) / 2; 
+  
+        // create two auxillary arrays 
+        int left[m - l + 1]; 
+        int right[r - m]; 
+  
+        // Store alternate array elements in left 
+        // and right subarray 
+        split(arr, left, right, l, m, r); 
+  
+        // Recurse first and second halves 
+        generateWorstCase(left, l, m); 
+        generateWorstCase(right, m + 1, r); 
+  
+        // join left and right subarray 
+        join(arr, left, right, l, m, r); 
+    } 
+} 
+
 /* Driver program to test above functions */
 int main() 
 { 
@@ -96,6 +143,7 @@ int main()
     for (int i=0; i<arr_size; i++)
         cin >> arr[i];
   
+    generateWorstCase(arr, 0, arr_size - 1); 
     // printf("Given array is \n"); 
     // printArray(arr, arr_size); 
 

896441cac63ce87db62f3f48b798b829297e7614
suraj <suraj.iitb.in@gmail.com>
2020-06-22 14:32:26 +0530

    Hypothesis testing on whole corpus

--- Baseline/Moss/Hypothesis/hyp.py
+++ Baseline/Moss/Hypothesis/hyp.py
@@ -3,6 +3,9 @@ b_max = 53
 i_min = 17
 i_max = 66
 
+b_avg = 30
+i_avg = 27
+
 with open('results.csv', 'r') as f:
 
     lines = [line.strip() for line in f]
@@ -32,6 +35,9 @@ with open('results.csv', 'r') as f:
             count1 = 0
             count2 = 0
 
+            max1 = -999
+            max2 = -999
+
             for j in range(lines_count):
             
                 line_b = lines[j].strip().split(',')
@@ -47,25 +53,40 @@ with open('results.csv', 'r') as f:
                         if code1 == code1_b:
                             sum1 += per1_b
                             count1 += 1
+
+                            if max1 < per1_b:
+                                max1 = per1_b
+
                         elif code1 == code2_b:
                             sum1 += per2_b
                             count1 += 1
+
+                            if max1 < per2_b:
+                                max1 = per2_b
 
                     if code2 not in bubble_processed:
                         if code2 == code1_b:
                             sum2 += per1_b
                             count2 += 1
+
+                            if max2 < per1_b:
+                                max2 = per1_b
+
                         elif code2 == code2_b:
                             sum2 += per2_b
                             count2 += 1
+
+                            if max2 < per2_b:
+                                max2 = per2_b
+                                
             if count1 > 0:
                 avg1 = sum1 / count1
-                bubble_processed[code1] = avg1
+                bubble_processed[code1] = {'avg': avg1, 'max': max1}
                 # if code1 == 'Data/BubbleSort/Dump/bubble-1673563.cpp':
                 #     print(sum1, count1, avg1)
             if count2 > 0:
                 avg2 = sum2 / count2
-                bubble_processed[code2] = avg2
+                bubble_processed[code2] = {'avg': avg2, 'max': max2}
                 # if code2 == 'Data/BubbleSort/Dump/bubble-1673563.cpp':
                 #     print(sum2, count2, avg2)
 
@@ -76,6 +97,9 @@ with open('results.csv', 'r') as f:
 
             count1 = 0
             count2 = 0
+
+            max1_i = -999
+            max2_i = -999
 
             for j in range(lines_count):
             
@@ -92,26 +116,40 @@ with open('results.csv', 'r') as f:
                         if code1 == code1_b:
                             sum1 += per1_b
                             count1 += 1
+
+                            if max1_i < per1_b:
+                                max1_i = per1_b
+
                         elif code1 == code2_b:
                             sum1 += per2_b
                             count1 += 1
+
+                            if max1_i < per2_b:
+                                max1_i = per2_b
 
                     if ('Data/BubbleSort/Dump/bubble' in code2 and code2 not in bubble_insertion_processed):
                         if code2 == code1_b:
                             sum2 += per1_b
                             count2 += 1
+
+                            if max2_i < per1_b:
+                                max2_i = per1_b
+
                         elif code2 == code2_b:
                             sum2 += per2_b
                             count2 += 1
 
+                            if max2_i < per2_b:
+                                max2_i = per2_b
+
             if count1 > 0:
                 avg1 = sum1 / count1
-                bubble_insertion_processed[code1] = avg1
+                bubble_insertion_processed[code1] = {'avg': avg1, 'max': max1_i}
                 # if code1 == 'Data/BubbleSort/Dump/bubble-1673563.cpp':
                 #     print(sum1, count1, avg1)
             if count2 > 0:
                 avg2 = sum2 / count2
-                bubble_insertion_processed[code2] = avg2
+                bubble_insertion_processed[code2] = {'avg': avg2, 'max':max2_i}
                 # if code2 == 'Data/BubbleSort/Dump/bubble-1673563.cpp':
                 #     print(sum2, count2, avg2)
             
@@ -121,17 +159,130 @@ with open('results.csv', 'r') as f:
     # print(bubble_insertion_processed['Data/BubbleSort/Dump/bubble-1673563.cpp'])
 
     with open('validate-hyp1.txt', 'w') as h1:
+        with open('validate-hyp2.txt', 'w') as h2:
+            with open('validate-hyp3.txt', 'w') as h3:
+                with open('validate-hyp4.txt', 'w') as h4:
+                    with open('validate-hyp5.txt', 'w') as h5:
 
-        for bubble in bubble_processed:
+                        for bubble in bubble_processed:
 
-            avg_with_bubble = bubble_processed[bubble]
+                            # hypothesis 1
+                            avg_with_bubble = bubble_processed[bubble]['avg']
 
-            try:
-                avg_with_insertion = bubble_insertion_processed[bubble]
-            except KeyError:
-                avg_with_insertion = 0
-            
-            if avg_with_bubble >= b_max and avg_with_insertion <= i_min:
-                h1.write(bubble+'\tValid\n')
-            else:
-                h1.write(bubble+'\tInvalid\n')
+                            try:
+                                avg_with_insertion = bubble_insertion_processed[bubble]['avg']
+                            except KeyError:
+                                avg_with_insertion = 0
+                            
+                            if avg_with_bubble >= b_max and avg_with_insertion <= i_min:
+                                h1.write(bubble+'\tValid\n')
+                            else:
+                                h1.write(bubble+'\tInvalid\n')
+
+                            # hypothesis 2
+                            max_with_bubble = bubble_processed[bubble]['max']
+
+                            try:
+                                max_with_insertion = bubble_insertion_processed[bubble]['max']
+                            except KeyError:
+                                max_with_insertion = 0
+                            
+                            if max_with_bubble >= b_max and max_with_insertion <= i_min:
+                                h2.write(bubble+'\tValid\n')
+                            else:
+                                h2.write(bubble+'\tInvalid\n')
+
+                            # hypothesis 3
+                            if max_with_bubble >= b_avg and max_with_insertion <= i_avg:
+                                h3.write(bubble+'\tValid\n')
+                            else:
+                                h3.write(bubble+'\tInvalid\n')
+
+                            # hypothesis 4                   
+                            if avg_with_bubble >= b_min and avg_with_insertion <= i_min:
+                                h4.write(bubble+'\tValid\n')
+                            else:
+                                h4.write(bubble+'\tInvalid\n')
+
+                            # hypothesis 5                 
+                            if max_with_bubble >= b_min and max_with_insertion <= i_min:
+                                h5.write(bubble+'\tValid\n')
+                            else:
+                                h5.write(bubble+'\tInvalid\n')
+
+    with open('validate-hyp1.txt', 'r') as h1:
+        valid = 0
+        invalid = 0
+        lines = [line.strip() for line in h1]
+        for line in lines:
+            line = line.split('\t')
+            # print(line[1])
+            if line[1] == 'Valid':
+                # print(line[1])
+                valid +=1
+            if line[1] == 'Invalid':
+                # print(line[1])
+                invalid +=1
+        val_per = valid / (valid +invalid) * 100
+        print('% Valid for hypothesis 1 is: ', val_per)
+    with open('validate-hyp2.txt', 'r') as h2:
+        valid = 0
+        invalid = 0
+        lines = [line.strip() for line in h2]
+        for line in lines:
+            line = line.split('\t')
+            # print(line[1])
+            if line[1] == 'Valid':
+                # print(line[1])
+                valid +=1
+            if line[1] == 'Invalid':
+                # print(line[1])
+                invalid +=1
+        val_per = valid / (valid +invalid) * 100
+        print('% Valid for hypothesis 2 is: ', val_per)
+    with open('validate-hyp3.txt', 'r') as h3:
+        valid = 0
+        invalid = 0
+        lines = [line.strip() for line in h3]
+        for line in lines:
+            line = line.split('\t')
+            # print(line[1])
+            if line[1] == 'Valid':
+                # print(line[1])
+                valid +=1
+            if line[1] == 'Invalid':
+                # print(line[1])
+                invalid +=1
+        val_per = valid / (valid +invalid) * 100
+        print('% Valid for hypothesis 3 is: ', val_per)
+    with open('validate-hyp4.txt', 'r') as h4:
+        valid = 0
+        invalid = 0
+        lines = [line.strip() for line in h4]
+        for line in lines:
+            line = line.split('\t')
+            # print(line[1])
+            if line[1] == 'Valid':
+                # print(line[1])
+                valid +=1
+            if line[1] == 'Invalid':
+                # print(line[1])
+                invalid +=1
+        val_per = valid / (valid +invalid) * 100
+        print('% Valid for hypothesis 4 is: ', val_per)
+    with open('validate-hyp5.txt', 'r') as h5:
+        valid = 0
+        invalid = 0
+        lines = [line.strip() for line in h5]
+        for line in lines:
+            line = line.split('\t')
+            # print(line[1])
+            if line[1] == 'Valid':
+                # print(line[1])
+                valid +=1
+            if line[1] == 'Invalid':
+                # print(line[1])
+                invalid +=1
+        val_per = valid / (valid +invalid) * 100
+        print('% Valid for hypothesis 5 is: ', val_per)
+                        

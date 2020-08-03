74ae0c9448714fedc5dd58d3d8d5df417ccaf74f
suraj <suraj.iitb.in@gmail.com>
2020-08-03 11:18:16 +0530

    Refactored code

--- Baseline/Moss/Hypothesis/hypothesis.py
+++ Baseline/Moss/Hypothesis/hypothesis.py
@@ -1,4 +1,4 @@
-def similarity(lines, lines_count):
+def similarity(lines, lines_count, algo1, algo2, algo1_algo1_processed, algo1_algo2_processed):
     for i in range(lines_count):
         
         line = lines[i].strip().split(',')
@@ -10,7 +10,7 @@ def similarity(lines, lines_count):
         
         # print(code1, per1, code2, per2)
 
-        if bubble in code1 and bubble in code2:
+        if algo1 in code1 and algo1 in code2:
 
             sum1 = 0
             sum2 = 0
@@ -30,9 +30,9 @@ def similarity(lines, lines_count):
                 code2_b = line_b[2]
                 per2_b = float(line_b[3][:-1])
             
-                if bubble in code1_b and bubble in code2_b:
+                if algo1 in code1_b and algo1 in code2_b:
                     
-                    if code1 not in bubble_processed:
+                    if code1 not in algo1_algo1_processed:
                         if code1 == code1_b:
                             sum1 += per1_b
                             count1 += 1
@@ -47,7 +47,7 @@ def similarity(lines, lines_count):
                             if max1 < per2_b:
                                 max1 = per2_b
 
-                    if code2 not in bubble_processed:
+                    if code2 not in algo1_algo1_processed:
                         if code2 == code1_b:
                             sum2 += per1_b
                             count2 += 1
@@ -64,16 +64,16 @@ def similarity(lines, lines_count):
                                 
             if count1 > 0:
                 avg1 = sum1 / count1
-                bubble_processed[code1] = {'avg': avg1, 'max': max1}
+                algo1_algo1_processed[code1] = {'avg': avg1, 'max': max1}
                 # if code1 == 'Data/BubbleSort/Dump/bubble-1673563.cpp':
                 #     print(sum1, count1, avg1)
             if count2 > 0:
                 avg2 = sum2 / count2
-                bubble_processed[code2] = {'avg': avg2, 'max': max2}
+                algo1_algo1_processed[code2] = {'avg': avg2, 'max': max2}
                 # if code2 == 'Data/BubbleSort/Dump/bubble-1673563.cpp':
                 #     print(sum2, count2, avg2)
 
-        if (bubble in code1 and insertion in code2) or (insertion in code1 and bubble in code2):
+        if (algo1 in code1 and algo2 in code2) or (algo2 in code1 and algo1 in code2):
 
             sum1 = 0
             sum2 = 0
@@ -93,9 +93,9 @@ def similarity(lines, lines_count):
                 code2_b = line_b[2]
                 per2_b = float(line_b[3][:-1])
             
-                if (bubble in code1_b and insertion in code2_b) or (insertion in code1_b and bubble in code2_b):
+                if (algo1 in code1_b and algo2 in code2_b) or (algo2 in code1_b and algo1 in code2_b):
                     
-                    if (bubble in code1 and code1 not in bubble_insertion_processed):
+                    if (algo1 in code1 and code1 not in algo1_algo2_processed):
                         if code1 == code1_b:
                             sum1 += per1_b
                             count1 += 1
@@ -110,7 +110,7 @@ def similarity(lines, lines_count):
                             if max1_i < per2_b:
                                 max1_i = per2_b
 
-                    if (bubble in code2 and code2 not in bubble_insertion_processed):
+                    if (algo1 in code2 and code2 not in algo1_algo2_processed):
                         if code2 == code1_b:
                             sum2 += per1_b
                             count2 += 1
@@ -127,145 +127,105 @@ def similarity(lines, lines_count):
 
             if count1 > 0:
                 avg1 = sum1 / count1
-                bubble_insertion_processed[code1] = {'avg': avg1, 'max': max1_i}
+                algo1_algo2_processed[code1] = {'avg': avg1, 'max': max1_i}
                 # if code1 == 'Data/BubbleSort/Dump/bubble-1673563.cpp':
                 #     print(sum1, count1, avg1)
             if count2 > 0:
                 avg2 = sum2 / count2
-                bubble_insertion_processed[code2] = {'avg': avg2, 'max':max2_i}
+                algo1_algo2_processed[code2] = {'avg': avg2, 'max':max2_i}
                 # if code2 == 'Data/BubbleSort/Dump/bubble-1673563.cpp':
                 #     print(sum2, count2, avg2)
 
-def hypothesis():
-    with open('hypothesis1.txt', 'w') as h1:
-        with open('hypothesis2.txt', 'w') as h2:
-            with open('hypothesis3.txt', 'w') as h3:
-                with open('hypothesis4.txt', 'w') as h4:
-                    with open('hypothesis5.txt', 'w') as h5:
+def hypothesis(suffix, algo1_algo1_processed, algo1_algo2_processed, algo1_max, algo1_min, algo1_avg, algo2_max, algo2_min, algo2_avg):
+    with open('hypothesis1'+suffix+'.txt', 'w') as h1:
+        with open('hypothesis2'+suffix+'.txt', 'w') as h2:
+            with open('hypothesis3'+suffix+'.txt', 'w') as h3:
+                with open('hypothesis4'+suffix+'.txt', 'w') as h4:
+                    with open('hypothesis5'+suffix+'.txt', 'w') as h5:
 
-                        for bubble in bubble_processed:
+                        for code in algo1_algo1_processed:
 
                             # hypothesis 1
-                            avg_with_bubble = bubble_processed[bubble]['avg']
+                            avg_with_algo1 = algo1_algo1_processed[code]['avg']
 
                             try:
-                                avg_with_insertion = bubble_insertion_processed[bubble]['avg']
+                                avg_with_algo2 = algo1_algo2_processed[code]['avg']
                             except KeyError:
-                                avg_with_insertion = 0
+                                avg_with_algo2 = 0
                             
-                            if avg_with_bubble >= b_max and avg_with_insertion <= i_min:
-                                h1.write(bubble+'\tValid\n')
+                            if avg_with_algo1 >= algo1_max and avg_with_algo2 <= algo2_min:
+                                h1.write(code+'\tValid\n')
                             else:
-                                h1.write(bubble+'\tInvalid\n')
+                                h1.write(code+'\tInvalid\n')
 
                             # hypothesis 2
-                            max_with_bubble = bubble_processed[bubble]['max']
+                            max_with_algo1 = algo1_algo1_processed[code]['max']
 
                             try:
-                                max_with_insertion = bubble_insertion_processed[bubble]['max']
+                                max_with_algo2 = algo1_algo2_processed[code]['max']
                             except KeyError:
-                                max_with_insertion = 0
+                                max_with_algo2 = 0
                             
-                            if max_with_bubble >= b_max and max_with_insertion <= i_min:
-                                h2.write(bubble+'\tValid\n')
+                            if max_with_algo1 >= algo1_max and max_with_algo2 <= algo2_min:
+                                h2.write(code+'\tValid\n')
                             else:
-                                h2.write(bubble+'\tInvalid\n')
+                                h2.write(code+'\tInvalid\n')
 
                             # hypothesis 3
-                            if max_with_bubble >= b_avg and max_with_insertion <= i_avg:
-                                h3.write(bubble+'\tValid\n')
+                            if max_with_algo1 >= algo1_avg and max_with_algo2 <= algo2_avg:
+                                h3.write(code+'\tValid\n')
                             else:
-                                h3.write(bubble+'\tInvalid\n')
+                                h3.write(code+'\tInvalid\n')
 
                             # hypothesis 4                   
-                            if avg_with_bubble >= b_min and avg_with_insertion <= i_min:
-                                h4.write(bubble+'\tValid\n')
+                            if avg_with_algo1 >= algo1_min and avg_with_algo2 <= algo2_min:
+                                h4.write(code+'\tValid\n')
                             else:
-                                h4.write(bubble+'\tInvalid\n')
+                                h4.write(code+'\tInvalid\n')
 
                             # hypothesis 5                 
-                            if max_with_bubble >= b_min and max_with_insertion <= i_min:
-                                h5.write(bubble+'\tValid\n')
+                            if max_with_algo1 >= algo1_min and max_with_algo2 <= algo2_min:
+                                h5.write(code+'\tValid\n')
                             else:
-                                h5.write(bubble+'\tInvalid\n')
+                                h5.write(code+'\tInvalid\n')
 
 
-def metric_calculate():
-    with open('hypothesis1.txt', 'r') as h1:
-        valid = 0
-        invalid = 0
-        lines = [line.strip() for line in h1]
-        for line in lines:
-            line = line.split('\t')
-            # print(line[1])
-            if line[1] == 'Valid':
+def metric_calculate(suffix1, suffix2):
+    for i in range(1,6):
+        with open('hypothesis' + str(i) + suffix1 + '.txt', 'r') as h1:
+            tp = 0
+            fp = 0
+            lines = [line.strip() for line in h1]
+            for line in lines:
+                line = line.split('\t')
                 # print(line[1])
-                valid +=1
-            if line[1] == 'Invalid':
-                # print(line[1])
-                invalid +=1
-        val_per = valid / (valid +invalid) * 100
-        print('% Recall for hypothesis 1 is: ', val_per)
-    with open('hypothesis2.txt', 'r') as h2:
-        valid = 0
-        invalid = 0
-        lines = [line.strip() for line in h2]
-        for line in lines:
-            line = line.split('\t')
-            # print(line[1])
-            if line[1] == 'Valid':
-                # print(line[1])
-                valid +=1
-            if line[1] == 'Invalid':
-                # print(line[1])
-                invalid +=1
-        val_per = valid / (valid +invalid) * 100
-        print('% Recall for hypothesis 2 is: ', val_per)
-    with open('hypothesis3.txt', 'r') as h3:
-        valid = 0
-        invalid = 0
-        lines = [line.strip() for line in h3]
-        for line in lines:
-            line = line.split('\t')
-            # print(line[1])
-            if line[1] == 'Valid':
-                # print(line[1])
-                valid +=1
-            if line[1] == 'Invalid':
-                # print(line[1])
-                invalid +=1
-        val_per = valid / (valid +invalid) * 100
-        print('% Recall for hypothesis 3 is: ', val_per)
-    with open('hypothesis4.txt', 'r') as h4:
-        valid = 0
-        invalid = 0
-        lines = [line.strip() for line in h4]
-        for line in lines:
-            line = line.split('\t')
-            # print(line[1])
-            if line[1] == 'Valid':
-                # print(line[1])
-                valid +=1
-            if line[1] == 'Invalid':
-                # print(line[1])
-                invalid +=1
-        val_per = valid / (valid +invalid) * 100
-        print('% Recall for hypothesis 4 is: ', val_per)
-    with open('hypothesis5.txt', 'r') as h5:
-        valid = 0
-        invalid = 0
-        lines = [line.strip() for line in h5]
-        for line in lines:
-            line = line.split('\t')
-            # print(line[1])
-            if line[1] == 'Valid':
-                # print(line[1])
-                valid +=1
-            if line[1] == 'Invalid':
-                # print(line[1])
-                invalid +=1
-        val_per = valid / (valid +invalid) * 100
-        print('% Recall for hypothesis 5 is: ', val_per)
+                if line[1] == 'Valid':
+                    # print(line[1])
+                    tp +=1
+                if line[1] == 'Invalid':
+                    # print(line[1])
+                    fp +=1
+
+        # with open('hypothesis' + str(i) + suffix2 + '.txt', 'r') as h1:
+        #     tn = 0
+        #     fn = 0
+        #     lines = [line.strip() for line in h1]
+        #     for line in lines:
+        #         line = line.split('\t')
+        #         # print(line[1])
+        #         if line[1] == 'Valid':
+        #             # print(line[1])
+        #             tn +=1
+        #         if line[1] == 'Invalid':
+        #             # print(line[1])
+        #             fn +=1
+
+        recall = tp / (tp +fp) * 100
+        # precision = tp / (tp +fn) * 100
+
+        print('% Recall for hypothesis' + str(i) + 'is: ', recall)
+        # print('% Precision for hypothesis' + str(i) + 'is: ', val_per)
+   
 
 
 
@@ -281,9 +241,9 @@ i_avg = 27
 bubble = 'Data/Bubble/Dump/bubble'
 insertion = 'Data/Insertion/Dump/insertion'
 
-bubble_processed = {}
+bubble_bubble_processed = {}
 bubble_insertion_processed = {}
-insertion_processed = {}
+insertion_insertion_processed = {}
 insertion_bubble_processed = {}
 
 with open('results.csv', 'r') as f:
@@ -295,6 +255,21 @@ with open('results.csv', 'r') as f:
     # print(bubble_processed['Data/BubbleSort/Dump/bubble-1673563.cpp'])
     # print(bubble_insertion_processed['Data/BubbleSort/Dump/bubble-1673563.cpp'])
 
-    similarity(lines, lines_count)
-    hypothesis()
-    metric_calculate()
+    # Bubble
+    algo1_algo1_processed = {}
+    algo1_algo2_processed = {}
+    similarity(lines, lines_count, bubble, insertion, algo1_algo1_processed, algo1_algo2_processed)
+    bubble_bubble_processed = algo1_algo1_processed
+    bubble_insertion_processed = algo1_algo2_processed
+    hypothesis("bubble", bubble_bubble_processed, bubble_insertion_processed, b_max, b_min, b_avg, i_max, i_min, i_avg)
+
+    # Insertion
+    # algo1_algo1_processed = {}
+    # algo1_algo2_processed = {}
+    # similarity(lines, lines_count, insertion, bubble, algo1_algo1_processed, algo1_algo2_processed)
+    # insertion_insertion_processed = algo1_algo1_processed
+    # insertion_bubble_processed = algo1_algo2_processed
+    # hypothesis("insertion", bubble_bubble_processed, bubble_insertion_processed)
+
+    # Metric Calculation
+    metric_calculate("bubble", "insertion")

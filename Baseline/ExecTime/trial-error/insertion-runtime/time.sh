09d9d240e3c0514c97f7e82699d930952d4c72c9
suraj <suraj.iitb.in@gmail.com>
2020-09-06 13:18:41 +0530

    Folder clean up & Input

--- Baseline/ExecTime/trial-error/insertion-runtime/time.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for i in 10 100 1000 10000 100000
-do
-    for j in {1..100}
-    do
-        (time ./insertion < ../../complexity-analysis/Input/input-$i) &>> insertion_$i
-    done
-done

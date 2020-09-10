f3b49d8226eb2afddb449109d1dd47806c4f1cf2
suraj <suraj.iitb.in@gmail.com>
2020-09-10 23:18:17 +0530

    Memory for iter quick

--- Execution/Memory/memory.sh
+++ Execution/Memory/memory.sh
@@ -3,20 +3,17 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 
-i=20000
-while [ "$i" -le 90000 ]
+for i in 10000 30000 50000 70000 100000 
 do
     echo $1 $i
     valgrind --tool=massif --time-unit=B --stacks=yes --massif-out-file=Results/${1}/${1}-${i} --max-snapshots=10 --detailed-freq=1 ./../Code/${1}_g < ../Input/input-${i}
     # ms_print Results/${1}/${1}-${i} > Results/${1}/${1}-${i}.read
-    i=$(( $i+10000 ))
 done
 
-i=20000
-while [ "$i" -le 90000 ]
+
+for i in 10000 30000 50000 70000 100000 
 do
     echo $1 $i
     # valgrind --tool=massif --time-unit=B --stacks=yes --massif-out-file=Results/${1}/${1}-${i} --max-snapshots=10 --detailed-freq=1 ./../Code/${1}_g < ../Input/input-${i}
     ms_print Results/${1}/${1}-${i} > Results/${1}/${1}-${i}.read
-    i=$(( $i+10000 ))
 done

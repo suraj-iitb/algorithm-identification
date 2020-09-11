666947d07120ba45ed8f20430b3fb0300bf0da1a
suraj <suraj.iitb.in@gmail.com>
2020-09-11 10:53:33 +0530

    Parser for mem dump

--- Execution/Memory/parser.py
+++ Execution/Memory/parser.py
@@ -1,11 +1,13 @@
 import os
 
 for d1 in os.listdir('Results'):
-    with open('Results/' + d1 + '/' + d1 + '-dump', 'w') as op:
+    with open('Results/' + d1 + '/' + d1 + '-dump.csv', 'w') as op:
         for file in os.listdir('Results/'+d1):
             if file.endswith('.read'):
 
                 lines = []
+                results = []
+                sum = 0.0
 
                 with open('Results/' + d1 + '/' + file) as f:
                     for line in f:
@@ -17,10 +19,16 @@ for d1 in os.listdir('Results'):
 
                 for i in range(len(lines)):
                     if lines[i][0] == 'n':
-                        if not file.startswith('quick-iter'):
-                            input = file[:-5].split('-')[1]
-                        else:
-                            input = file.split('-')[2]
-                        if input in ['10000', '50000', '100000']:
-                            op.write(input + ',' + ','.join(lines[i+2]) + '\n')
-    break
+                        results.append(lines[i+2])
+
+                for r in results:
+                    r = float(r[2])
+                    sum += r
+                sum /= len(results)
+                
+                if not file.startswith('quick-iter'):
+                    input = file[:-5].split('-')[1]
+                else:
+                    input = file.split('-')[2]
+                if input in ['10000', '50000', '100000']:
+                    op.write(input + ',' + str(sum) + '\n')

8bcae37a40e2a967931947429cf86bc93be79a0e
suraj <suraj.iitb.in@gmail.com>
2020-10-11 21:11:45 +0530

    Insetion ast

--- MOSS/Data/Insertion/Dump/C/insertion2411611.c
+++
@@ -1,1 +0,0 @@
-a[100],v,j,n;p(){for(v=0;v<n;printf("%d%s",a[v++],v-n+1?" ":"\n"));}main(i){for(scanf("%d",&n);j<n;scanf("%d",a+j++));for(p();i<n;a[j+1]=v,p())for(j=i-1,v=a[i++];a[j]>v;a[j+1]=a[j--]);exit(0);}

3514642fc6c3b69c07a683658757c4a90a6f20ec
suraj <suraj.iitb.in@gmail.com>
2020-10-11 22:21:23 +0530

    bubble Ins Results

--- Kernels/Data/AST/data-gen.py
+++ Kernels/Data/AST/data-gen.py
@@ -5,28 +5,35 @@ for i in range(0, len(algo)):
     while j < len(algo):
         print(algo[i], algo[j])
         with open(algo[i]+'_'+algo[j]+'.train', 'w') as ans:
-            # Algo1
-            count = 0
-            with open(algo[i]+'.ast', 'r') as f:
-                lines = f.read().split('\n')
-                for line in lines:
-                    count += 1
-                    line = line.split(' ')
-                    line[0] = '1'
-                    ans.write(' '.join(line)+'\n')
-                    if (count == 500):
-                        break
-            # Algo2
-            count = 0
-            with open(algo[j]+'.ast', 'r') as f:
-                lines = f.read().split('\n')
-                for line in lines:
-                    count += 1
-                    line = line.split(' ')
-                    line[0] = '-1'
-                    ans.write(' '.join(line)+'\n')
-                    if (count == 500):
-                        break
+            with open(algo[i]+'_'+algo[j]+'.test', 'w') as ans1:
+                # Algo1
+                count = 0
+                with open(algo[i]+'.ast', 'r') as f:
+                    lines = f.read().split('\n')
+                    for line in lines:
+                        count += 1
+                        line = line.split(' ')
+                        line[0] = '1'
+                        if (count <=500):
+                            ans.write(' '.join(line)+'\n')
+                        elif (count > 500 and count <= 600):
+                            ans1.write(' '.join(line)+'\n')
+                        elif (count > 600):
+                            break
+                # Algo2
+                count = 0
+                with open(algo[j]+'.ast', 'r') as f:
+                    lines = f.read().split('\n')
+                    for line in lines:
+                        count += 1
+                        line = line.split(' ')
+                        line[0] = '-1'
+                        if (count <=500):
+                            ans.write(' '.join(line)+'\n')
+                        elif (count > 500 and count <= 600):
+                            ans1.write(' '.join(line)+'\n')
+                        elif (count > 600):
+                            break
         j += 1
         break
     break

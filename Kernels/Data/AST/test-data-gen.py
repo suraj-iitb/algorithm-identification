2faee84c09e33a52dc493f97f07164b4158a992e
suraj <suraj.iitb.in@gmail.com>
2020-10-16 09:58:05 +0530

    skewed data set

--- Kernels/Data/AST/test-data-gen.py
+++ Kernels/Data/AST/test-data-gen.py
@@ -1,10 +1,12 @@
+import random
+
 algo = ['bubble', 'insertion', 'selection', 'counting', 'merge', 'quick']
 
-with open('algo.test', 'w') as ans:
+with open('algo-r2.test', 'w') as ans:
     for i in range(0, len(algo)):
         with open(algo[i]+'.ast', 'r') as f:
             lines = f.read().split('\n')
-            for line in lines[501:531]:
+            for line in lines[701: 701+random.randrange(80)]:
                 line = line.split(' ')
                 line[0] = str(i)
                 ans.write(' '.join(line)+'\n')

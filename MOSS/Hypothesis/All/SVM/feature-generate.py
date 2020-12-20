58106a151386dc13cc7b9cc43ab3efe2c8d2cb36
suraj <suraj.iitb.in@gmail.com>
2020-12-20 17:21:21 +0530

    feat for svm

--- MOSS/Hypothesis/All/SVM/feature-generate.py
+++ MOSS/Hypothesis/All/SVM/feature-generate.py
@@ -14,39 +14,47 @@ with open('../Results/all/100/100_svm.csv', 'r') as s:
             c = []
             m = []
             q = []
+            temp = []
 
             for i in range(0, len(sim), 2):
                 if sim[i].startswith('bubble'):
-                    b.append(int(sim[i+1]))
+                    b.append(sim[i+1])
                 elif sim[i].startswith('insertion'):
-                    ins.append(int(sim[i+1]))
+                    ins.append(sim[i+1])
                 elif sim[i].startswith('selection'):
-                    s.append(int(sim[i+1]))
+                    s.append(sim[i+1])
                 elif sim[i].startswith('counting'):
-                    c.append(int(sim[i+1]))
+                    c.append(sim[i+1])
                 elif sim[i].startswith('merge'):
-                    m.append(int(sim[i+1]))
+                    m.append(sim[i+1])
                 elif sim[i].startswith('quick'):
-                    q.append(int(sim[i+1]))
+                    q.append(sim[i+1])
 
-            b.sort(reverse=True)
-            ins.sort(reverse=True)
-            s.sort(reverse=True)
-            c.sort(reverse=True)
-            m.sort(reverse=True)
-            q.sort(reverse=True)
-            print(b, ins, s, c, m, q)
+            temp.extend(b)
+            temp.extend(ins)
+            temp.extend(s)
+            temp.extend(c)
+            temp.extend(m)
+            temp.extend(q)
+            # b.sort(reverse=True)
+            # ins.sort(reverse=True)
+            # s.sort(reverse=True)
+            # c.sort(reverse=True)
+            # m.sort(reverse=True)
+            # q.sort(reverse=True)
+            # print(b, ins, s, c, m, q)
 
             ans.write(line[0].strip())
-            for algo in [b, ins, s, c, m, q]:
-                count = 0
-                for i in algo:
-                    ans.write(','+str(i))
-                    count += 1
-                    if(count == 10):
-                        break
-                for i in range(count, 10):
-                    ans.write(',0')
+            # for algo in [b, ins, s, c, m, q]:
+            #     count = 0
+            #     for i in algo:
+            #         ans.write(','+str(i))
+            #         count += 1
+            #         if(count == 10):
+            #             break
+            #     for i in range(count, 10):
+            #         ans.write(',0')
+            ans.write(','.join(temp))
             ans.write('\n')
 
             

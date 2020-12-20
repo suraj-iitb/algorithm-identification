6f143a9b952751b288e4ad4e35d71eb35c94ab39
suraj <suraj.iitb.in@gmail.com>
2020-12-20 12:12:41 +0530

    feature extraction for svm

--- MOSS/Hypothesis/All/SVM/feature-extract.py
+++ MOSS/Hypothesis/All/SVM/feature-extract.py
@@ -11,31 +11,32 @@ with open('../Results/all/100/100.csv', 'r') as f:
 
 with open('../Results/all/100/100_svm.csv', 'w') as s:
     for algo in os.listdir('../../../Data'):
-        count = 1
-        limit = random.randrange(100)
-        print(algo, limit)
-        for code in os.listdir('../../../Data/' + algo + '/Dump'):
-            if not code.endswith('.txt') and not code.endswith('C'):
-                temp = []
-                if count <= limit:
-                    # print(code)
-                    for line in lines:
-                        line = line.strip().split(',')
-                        if line[0] == code:
-                            # print(line[0])
-                            temp.append(line[2])
-                            temp.append(line[1])
-                        if line[2] == code:
-                            # print(line[2])
-                            temp.append(line[0])
-                            temp.append(line[3])
-                    s.write(code + ',' + ','.join(temp) + '\n')
-                    count += 1
-                else:
-                    break
-        # break
+        if not algo.endswith('.txt'):
+            count = 1
+            limit = random.randrange(100)
+            print(algo, limit)
+            for code in os.listdir('../../../Data/' + algo + '/Dump/CPP'):
+                if not code.endswith('.txt'):
+                    temp = []
+                    if count <= limit:
+                        # print(code)
+                        for line in lines:
+                            line = line.strip().split(',')
+                            if line[0] == code:
+                                # print(line[0])
+                                temp.append(line[2])
+                                temp.append(line[1])
+                            if line[2] == code:
+                                # print(line[2])
+                                temp.append(line[0])
+                                temp.append(line[3])
+                        s.write(code + ',' + ','.join(temp) + '\n')
+                        count += 1
+                    else:
+                        break
+            # break
 
-        
+            
 
 
 
